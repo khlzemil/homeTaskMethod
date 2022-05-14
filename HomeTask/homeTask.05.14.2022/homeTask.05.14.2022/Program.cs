bcb1811e4ac4b1573782c75ee0a7354bae55bfab
khlzemil <emilmg@code.edu.az>
2022-05-15 01:11:52 +0400

    Added hometask

---
+++ HomeTask/homeTask.05.14.2022/homeTask.05.14.2022/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+
+    public static void Main()
+    {
+        if (SumArray(12, 13, 14))
+            {
+            Console.WriteLine("True");
+            }
+        else 
+            { 
+            Console.WriteLine("False");
+            }
+    }
+
+    public static bool SumArray(params int[] arr)
+    {
+        int sum = 0;
+        foreach (int element in arr)
+        {
+            sum += element;
+
+        }
+        if (sum % 2 == 0)
+            return true;
+        return false;
+
+
+    } 
+
+}
---
+++ HomeTask/hometask2.05.14.2022/hometask2.05.14.2022/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+
+{
+
+    public static void Main()
+    {
+        Pyramid(5, 5);
+
+    }
+
+
+    public static void Pyramid( int en, int uzunluq)
+    {
+        for (int i = 1; i <= en; i++)
+        {
+            for(int j = 1; j <= i; j++)
+            {
+                Console.Write("* ");
+
+            }
+            Console.WriteLine("");
+        }
+
+
+    }
+}
